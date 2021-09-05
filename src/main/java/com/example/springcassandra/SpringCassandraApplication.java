ed14a669db68323d5a105368a6c04cd77da2ebd3
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-09-05 22:41:47 +0530

    Initial Commit

---
+++ src/main/java/com/example/springcassandra/SpringCassandraApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCassandraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCassandraApplication.class, args);
+    }
+
+}
